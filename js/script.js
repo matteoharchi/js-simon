a64c06111dd3099a70b55c99547b242a92cfcd3c
Matteo Harchi <matteo.harchi@gmail.com>
2020-07-28 09:42:07 +0200

    corretta funzione random

--- js/script.js
+++ js/script.js
@@ -38,5 +38,5 @@ setTimeout(function(){
 //funzione che crea un numero Random
 
 function randomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min - 1)+1);
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
